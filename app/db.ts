730e1784b1a16071f2e0ce5a467f01b414cd7048
DanielVNZ <123446671+DanielVNZ@users.noreply.github.com>
2025-07-14 18:51:53 +1200

    Update db.ts

--- app/db.ts
+++ app/db.ts
@@ -8,7 +8,12 @@ import { getDownloadUrl } from 'app/utils/r2';
 // Optionally, if not using email/pass login, you can
 // use the Drizzle adapter for Auth.js / NextAuth
 // https://authjs.dev/reference/adapter/drizzle
-let client = postgres(`${process.env.POSTGRES_URL!}?sslmode=require`);
+let client = postgres(process.env.POSTGRES_URL!, {
+  ssl: false,
+  max: 1,
+  idle_timeout: 20,
+  connect_timeout: 10
+});
 let db = drizzle(client);
 
 // Export client for use in other files
